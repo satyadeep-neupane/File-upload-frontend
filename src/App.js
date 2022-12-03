be4cdcaa71a821ad4c28ed352dc69dd39684b000
satyadeep-neupane <satyadeep.neupane@deerwalk.edu.np>
2022-12-03 17:31:52 +0545

    File upload frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [email, setEmail] = useState('');
+  const [photo, setPhoto] = useState('');
+
+  const handleForm = async (e) => {
+    e.preventDefault();
+    console.log(email, photo);
+
+    const formData = new FormData();
+    formData.append('email', email);
+    formData.append('photo', photo);
+
+    const res = await axios.post('http://localhost:5000/', formData);
+    console.log(res.data);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleForm}>
+        <div>
+          <label>Email:</label>
+          <input type="text" name="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        </div>
+
+        <div>
+          <label>Photo:</label>
+          <input type="file" name="photo" onChange={(e) => setPhoto(e.target.files[0])} />
+        </div>
+
+        <button>Add User</button>  
+      </form>
     </div>
   );
 }
